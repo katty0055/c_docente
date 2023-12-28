010da822da5642e4bb462b2bc57e6b754af5343e
katty0055 <katteryne0055@fpuna.edu.py>
2023-12-28 17:32:21 -0300

    cambios en route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,14 @@
 //componente donde estan las rutas
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-//import PostulacionForm from "../views/Postulacion/PostulacionForm";
 import Postulacion from "../views/Postulacion";
 import Pagina from "../views/Pagina/Pagina";
 import CargarDocumentos from "../views/CargarDocumentos";
-//import ConcursoCard from "../components/ConcursosCard/ConcursoCard";
 import Login from "../views/Inicio";
 import CrearConcurso from "../views/Concurso/CrearConcurso";
 import AgregarRequisitos from "../views/Concurso/AgregarRequisitos";
-
-//import ConcursoCreado from "../views/Concurso/ConcursoCreado";
 import Editar from "../views/Concurso/editar";
 import Postulaciones from "../views/RecuperarPostulaciones";
 import CarpetaContenido from "../views/RecuperarPostulaciones/CarpetaContenido";
-//import Documento from "../views/RecuperarPostulaciones/PDFViewer";
 
 const router = createBrowserRouter([
     // Definición de rutas
@@ -36,10 +31,6 @@ const router = createBrowserRouter([
             {
                 path: 'recuperar_postulaciones/',
                 element: <Postulaciones/>,
-                //children: [
-                   
-                //]
-                
             },
             {
                 path: 'contenido',
@@ -49,45 +40,20 @@ const router = createBrowserRouter([
                 path: 'crear_concurso/',
                 element: <CrearConcurso/>,
             },
-             {
+            {
                 path: 'agregar_requisitos_concurso/',
                 element: <AgregarRequisitos/>
-             },
-            // {
-            //     path: 'postulacion/',
-            //     element: <Postulacion/>
-            // }, 
-           
+            },           
         ],
     },
     {
         path: 'cargar_documentos',
         element: <CargarDocumentos/>
-    },
-
-   
-    
-
-    // {
-    //     path: 'postulacion/', // Agrega el parámetro dinámico
-    //     element: <PostulacionForm/>
-    // },
-
-    // {
-    //     path: 'postulacion2/',
-    //     element: <Postulacion/>
-    // },
-   
-     ,
-     {
-        //  path: 'editar_concurso/:concurso_id',
-        //  element: <EditarConcurso/>,
+    },     
+    {
         path: 'editar_concurso/',
         element: <Editar/>,
-        },
-    
-        
-    
+    },
 ]);
 
 const MyRoutes = () => {
